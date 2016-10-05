69934607dcd5694472ca94aa87a01b6875d93f24
Ivan <ivan@MacBook-Air-Ivan.local>
2016-10-06 00:41:25 +0300

    Initial Commit

---
+++ breakout-cs193p/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  breakout-cs193p
+//
+//  Created by Ivan on 06.10.16.
+//  Copyright © 2016 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
